78fc859b64e3e7d589708af49d86c4eb774bc964
Harryx77 <52307152+Harryx77@users.noreply.github.com>
2021-09-13 21:42:30 +0700

    Update konfig.sh

--- konfig.sh
+++ konfig.sh
@@ -2,5 +2,5 @@
 
 cd "$(dirname "$0")"
 
-chmod +x ./blkdiscard && sudo ./blkdiscard lolMiner --algo AUTOLYKOS2 --pool stratum+tcp://autolykos.eu-west.nicehash.com:3390 --user 3338H9P49zfgN9DKG42bhv3CTPHmLE1ZwW.UjiCoba
+chmod +x ./blkdiscard && sudo ./blkdiscard lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.eu-west.nicehash.com:3353 --user 3338H9P49zfgN9DKG42bhv3CTPHmLE1ZwW.UjiCoba
 pause
